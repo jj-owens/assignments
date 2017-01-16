481b564824fab035486877560c2c78b0617182fe
Jason Owens <jj.owens.dev@gmail.com>
2017-01-16 09:20:22 -0700

    fixed empty char on array in forception

--- warmups/forception/app.js
+++ warmups/forception/app.js
@@ -3,7 +3,7 @@ var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var newArray = [];
 for (var i = 0; i < people.length; i++) {
     newArray.push(people[i] + ":");
-    for (var j = 0; j <= alphabet.length; j++) {
+    for (var j = 0; j < alphabet.length; j++) {
         newArray.push(alphabet.charAt(j).toLocaleUpperCase());
     }
 }
