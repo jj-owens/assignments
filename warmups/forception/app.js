7fe2734c6cd7e1acd29fa05d3aa4dc7cc43d212e
Jason Owens <jj.owens.dev@gmail.com>
2017-01-16 09:17:01 -0700

    added forception warmup

---
+++ warmups/forception/app.js
@@ -0,0 +1,10 @@
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var newArray = [];
+for (var i = 0; i < people.length; i++) {
+    newArray.push(people[i] + ":");
+    for (var j = 0; j <= alphabet.length; j++) {
+        newArray.push(alphabet.charAt(j).toLocaleUpperCase());
+    }
+}
+console.log(newArray);
