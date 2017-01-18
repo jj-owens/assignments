2c0ffceaf838ddd1428af9b236a4a3ca64d730ec
Jason Owens <jj.owens.dev@gmail.com>
2017-01-18 12:52:26 -0700

    mario project fixes and tablit fixed

---
+++ exercises/luigiboot/app.js
@@ -0,0 +1,3 @@
+/**
+ * Created by vschool2 on 1/17/17.
+ */
--- projects/mariopest/app.js
+++ projects/mariopest/app.js
@@ -1,16 +1,29 @@
-function addGoomba() {
+var goombaNum = 0;
+var cheepNum = 0;
+var ombNum = 0;
+var goombaPrice = 5;
+var ombPrice = 11;
+var cheepPrice = 7;
+var totalPrice = 0;
 
+
+function addGoomba() {
+    goombaNum++;
+    document.getElementById("badAdd3").innerHTML = goombaNum;
+    //totalPrice += goombaNum * goombaPrice;
 }
 
 function addBobOmbs() {
-
+    ombNum++;
+    document.getElementById("badAdd1").innerHTML = ombNum;
 }
 
 function addCheepCheeps() {
-
+    cheepNum++;
+    document.getElementById("badAdd2").innerHTML = cheepNum;
 }
 
-var totalPrice = 0
-var goombaPrice = 5
-var bobOmbs = 7
-
+function getTotal() {
+    totalPrice = (goombaNum * goombaPrice) + (cheepNum * cheepPrice) + (ombNum * ombPrice);
+    document.getElementById("calculatedText").innerHTML = totalPrice;
+}
