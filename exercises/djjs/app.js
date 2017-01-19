4f4a1cf32585c2b2fe9555244d8c0ebedee7dd74
Jason Owens <jj.owens.dev@gmail.com>
2017-01-19 12:31:57 -0700

    fixed dj box keypress response

--- exercises/djjs/app.js
+++ exercises/djjs/app.js
@@ -14,4 +14,18 @@ function colorChange3() {
     document.getElementById("colorblock").className = "colorblock4";
 }
 
-document.getElementById("bodyId").addEventListener("wheel", colorChange3);
+document.addEventListener("wheel", colorChange3);
+document.addEventListener("keydown", function (event) {
+    if (event.which == 66) {
+        document.getElementById("colorblock").className = "toBlue";
+    } else if (event.which == 82) {
+        document.getElementById("colorblock").className = "toRed";
+    } else if (event.which == 89) {
+        colorChange();
+    } else if (event.which == 71) {
+        colorChange2();
+    } else if (event.which == 79) {
+        colorChange3();
+    }
+    console.log(event.which);
+});
