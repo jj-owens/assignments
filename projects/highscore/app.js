f049ae0938f735d7f3ff4311629fc47d47c43012
Jason Owens <jj.owens.dev@gmail.com>
2017-01-28 02:00:34 -0700

    added changes

--- projects/highscore/app.js
+++ projects/highscore/app.js
@@ -1,53 +1,52 @@
-/*// Save something to web storage:
-localStorage.setItem('key', 'value');
+   $(document).ready(function () {
+       $("#scoreTable").tablesorter();
+   });
+   var scoreArray = [];
 
-// Get something from web storage that  was previously saved:
-var myValue = localStorage.getItem('key');
+   for (var i = 0; i < localStorage.length; i++) {
+       var temp = JSON.parse(localStorage.getItem("newScore"));
+       $('#scoreTable').append(temp);
+   }
 
-// Remove an key/value pair from localStorage:
-localStorage.removeItem('key');
-
-/!*It's important to keep in mind that web storage only stores strings of data. ' +
-'So if you need to store or retrieve more complex data types, you can use JSON.stringify() ' +
-'to turn complex data into a string before saving it to web storage or JSON.parse() to turn something ' +
-'back into a complex data type before trying to display or manipulate it.*!/*/
-/*var inputData = document.getElementById("formData");
-var firstName = inputData.elements["firstNameInput"].value;
-var lastName = inputData[1].value;
-var gameName = inputData[2].value;
-var highScore = inputData[3].value;
-var email = inputData[4].value;
-var date = inputData[5].value;
-console.log(inputData);
-console.log(firstName);
-var trash = document.querySelector('.form-check-input:checked');*/
-
-function formSubmit() {
-    var inputData = document.getElementById("formData").elements;
-    var firstName = inputData["firstNameInput"].value;
-    var lastName = inputData["lastNameInput"].value;
-    var gameName = inputData["gameInput"].value;
-    var highScore = inputData["scoreInput"].value;
-    var email = inputData["emailInput"].value;
-    var date = inputData["dateInput"].value;
-    console.log(inputData);
-    console.log(firstName);
-    var trash = document.querySelector('.form-check-input:checked');
-
-    var submittedScore = new NewScore(firstName, lastName, gameName, highScore, email, date, trash)
-    localStorage.setItem("newScore", JSON.stringify(submittedScore));
-    $('scoreTable tr:last').after('<tr><td>my data</td><td>more data</td></tr>');
-}
-
-
-function NewScore(firstname, lastname, gamename, highscore, email, date, trash) {
-    this.firstName = firstname;
-    this.lastName = lastname;
-    this.gameName = gamename;
-    this.highScore = highscore;
-    this.email = email;
-    this.date = date;
-    this.trash = trash;
-}
+   function formSubmit() {
+       var inputData = document.getElementById("formData").elements;
+       var firstName = inputData["firstNameInput"].value;
+       var lastName = inputData["lastNameInput"].value;
+       var gameName = inputData["gameInput"].value;
+       var highScore = inputData["scoreInput"].value;
+       var email = inputData["emailInput"].value;
+       var date = inputData["dateInput"].value;
+       var trash = document.querySelector('.form-check-input:checked');
+       trash = "off";
+       if (trash.value = "on") {
+           var trashTalk = ["Is that all you've got?",
+                         "I've seen better scores from a rat!",
+                         "There is no spoon.",
+                        "Get out of my house!",
+                        "Too easy.",
+                        "Pwnd!",
+                        "I own you bro!",
+                        "Peace, son!",
+                        "Talk to the hand..",
+                        "It gets no better, I'm too clean!"];
+           var roll = Math.floor(Math.random() * (10 - 0) + 0);
+           trash = trashTalk[roll];
+       } else {
+           trash = "This is my new score.";
+       }
+       var submittedScore = new NewScore(firstName, lastName, gameName, highScore, email, date, trash);
+       scoreArray.push(submittedScore);
+       localStorage.setItem("newScore", JSON.stringify(scoreArray));
+       $('#scoreTable').append(`<tr><td>${submittedScore.firstName}</td><td>${submittedScore.lastName}</td><td>${submittedScore.gameName}</td><td>${submittedScore.highScore}</td><td>${submittedScore.email}</td><td>${submittedScore.date}</td><td>${submittedScore.trash}</td></tr>`);
+   }
 
 
+   function NewScore(firstname, lastname, gamename, highscore, email, date, trash) {
+       this.firstName = firstname;
+       this.lastName = lastname;
+       this.gameName = gamename;
+       this.highScore = highscore;
+       this.email = email;
+       this.date = date;
+       this.trash = trash;
+   }
