312bf5d25a5fee029b8f4a23e29c06436dfd34c5
Jason Owens <jj.owens.dev@gmail.com>
2017-02-04 15:28:30 -0700

    finished ugly things website

--- exercises/uglythings/app.js
+++ exercises/uglythings/app.js
@@ -2,6 +2,10 @@ var app = angular.module("uglyApp", []);
 
 app.controller("MainController", ['$scope', function($scope){
     $scope.itemArray = [];
+        $scope.addItem = function (item){
+            $scope.itemArray.push(item);
+            console.log($scope.itemArray);
+            $scope.item = {};
+        }
     
-    
-}])
+}]);
