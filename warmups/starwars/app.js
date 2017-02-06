63a870aa137294ca474c2790ef0afef3f1d581e0
Jason Owens <jj.owens.dev@gmail.com>
2017-02-06 10:18:00 -0700

    added starwars warmup

---
+++ warmups/starwars/app.js
@@ -0,0 +1,13 @@
+var app = angular.module("App", []);
+var audio = document.getElementById("sound");
+app.controller("MainController", ["$scope", "$http", function($scope, $http){
+    $http.get("http://swapi.co/api/films/1/").then(function (response){
+        $scope.data = response.data;
+        console.log ($scope.data.opening_crawl);
+        $scope.text = $scope.data.opening_crawl;
+        sound.play();
+    }
+    
+    )
+}]);
+
