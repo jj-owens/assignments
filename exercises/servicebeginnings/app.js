0971f55c62730a97a6332a7ed7d2f0bd56cd5235
Jason Owens <jj.owens.dev@gmail.com>
2017-02-08 10:34:46 -0700

    fixed delete button, added filter for null values on images

--- exercises/servicebeginnings/app.js
+++ exercises/servicebeginnings/app.js
@@ -1,24 +1,30 @@
 angular.module("myApp", [])
-.controller("MainController", ["$scope", "pokeService", function ($scope, pokeService){
-   $scope.pokemons = [];
-    console.log($scope.pokeName);
-    
-$scope.addPokemon = function (name) {
-        
-        console.log($scope.pokemons);
-        pokeService.getImageData(name).then(function(sprites){
-    $scope.pokemons.push({name: name.toUpperCase(), images: sprites});
-    });
+    .controller("MainController", ["$scope", "pokeService", function ($scope, pokeService) {
+        $scope.pokemons = [];
+        $scope.addPokemon = function (name) {
+            pokeService.getImageData(name).then(function (sprites) {
+                $scope.pokemons.push({
+                    name: name.toUpperCase(),
+                    images: sprites
+                });
+            });
+        }
+        $scope.remPokemon = function (input) {
+            for (var i = 0; i < $scope.pokemons.length; i++) {
+                if ($scope.pokemons[i].name === input.toUpperCase()) {
+                    $scope.pokemons.splice(i, 1);
+                }
+            }
+        }
+
+}])
+
+.service("pokeService", ["$http", function ($http) {
+
+    this.getImageData = function (name) {
+        return $http.get("http://pokeapi.co/api/v2/pokemon/" + name).then(function (response) {
+            return response.data.sprites
+        })
+
     }
 }])
-
-.service("pokeService", ["$http", function($http){
-    
-    this.getImageData = function(name){
-    return $http.get("http://pokeapi.co/api/v2/pokemon/" + name).then(function(response){
-        return response.data.sprites;
-    })
-        
-}
-}])
-
