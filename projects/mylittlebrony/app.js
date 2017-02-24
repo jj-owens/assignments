71bee58f7e6f1854bdd867eb9702c00cb513a881
Jason Owens <jj.owens.dev@gmail.com>
2017-02-24 13:50:23 -0700

    mylittlebrony update

---
+++ projects/mylittlebrony/app.js
@@ -0,0 +1,40 @@
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.controller("MainController", ["$scope", "$route", "MainService", function ($scope, $route, MainService) {
+
+	MainService.getFavList().then(function (data) {
+		$scope.ponies = data;
+	});
+
+	$scope.delBtn = function (id) {
+		MainService.delPony(id).then(function (data) {
+			MainService.getFavList().then(function (data) {
+				$scope.ponies = data;
+			});
+
+		});
+
+		$route.reload();
+	};
+	$scope.submitForm = function (form) {
+		alert("Submitting Pony!");
+		MainService.ponyValidator(form).then(function (response) {
+			MainService.getFavList().then(function (data) {
+				$scope.ponies = data;
+				window.location.replace("#!/ponies");
+				$route.reload();
+			});
+		});
+
+	}
+
+}])
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when("/home", {
+			templateUrl: "home.html"
+		})
+		.when("/ponies", {
+			templateUrl: "ponies.html"
+		});
+});
---
+++ projects/mylittlebrony/service.js
@@ -0,0 +1,37 @@
+angular.module("myApp")
+	.service("MainService", ["$http", function ($http) {
+		var re = /^[A-Za-z]+$/;
+		var self = this;
+		this.addPony = function (input) {
+			return $http.post("http://api.vschool.io/jjowens/pony", input).then(function (response) {
+				alert("Added Pony to Collection!");
+				return response.data;
+			});
+		}
+
+		this.delPony = function (id) {
+			return $http.delete("http://api.vschool.io/jjowens/pony/" + id).then(function (response) {
+				return response.data;
+			});
+		};
+		this.getFavList = function () {
+			return $http.get("http://api.vschool.io/jjowens/pony").then(function (response) {
+				console.log(response.data);
+				return response.data;
+			});
+		};
+		this.ponyValidator = function (input) {
+			$http.get("http://api.vschool.io/jjowens/pony").then(function (response) {
+				var ponyList = response.data;
+			});
+			if (Object.keys(input).length < 3) {
+				return alert("Incomplete Pony Information!");
+			} else if (input.name.length < 3) {
+				return alert("Pony name must be at least 3 letters!");
+			} else if (re.test(input.name) != true) {
+				return alert("Pony name cannot contain numbers!");
+			} else {
+				return self.addPony(input);
+			}
+		};
+}]);
